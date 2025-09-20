668adf16b6b85d4594ab094386c3eaf1e956d2d7
Matheus Pereira <matheus.p.soares242@gmail.com>
2025-09-20 11:40:02 -0300

    init classe SistemaDelivery

---
+++ src/Core/SistemaDelivery.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Core;
+
+use App\Entity\Cliente;
+use App\Entity\Entregador;
+use App\Entity\Restaurante;
+use App\Entity\Produto;
+use App\Entity\Pedido;
+use App\Entity\ItemPedido;
+
+class SistemaDelivery
+{
+    
+}
